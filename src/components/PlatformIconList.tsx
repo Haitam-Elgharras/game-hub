dfef6cc7d02b1bd076cc155bddeb07386058ba10
Haitam Elgharras <elgharras.haitam@gmail.com>
2023-06-22 19:22:00 +0100

    Display platform icons

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,48 @@
+import {
+  FaWindows,
+  FaXbox,
+  FaPlaystation,
+  FaLinux,
+  FaApple,
+  FaAndroid,
+} from "react-icons/fa";
+import { SiNintendo } from "react-icons/si";
+import { MdPhoneIphone } from "react-icons/md";
+import { BsGlobe } from "react-icons/bs";
+
+import { Platform } from "../hooks/useGames";
+import { HStack, Icon, Text } from "@chakra-ui/react";
+import { IconType } from "react-icons";
+
+interface Props {
+  platforms: Platform[];
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    xbox: FaXbox,
+    playstation: FaPlaystation,
+    linux: FaLinux,
+    mac: FaApple,
+    android: FaAndroid,
+    ios: MdPhoneIphone,
+    nintendo: SiNintendo,
+    web: BsGlobe,
+  };
+
+  return (
+    <HStack marginTop={2} marginBottom={1}>
+      {platforms.map((platform) => (
+        <Icon
+          key={platform.id}
+          as={iconMap[platform.slug]}
+          fontSize={"lg"}
+          color={"gray.500"}
+        />
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;
