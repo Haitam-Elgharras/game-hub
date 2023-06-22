0dcba70c33545398199fff3b2f1cf59cbb60f5d3
Haitam Elgharras <elgharras.haitam@gmail.com>
2023-06-22 13:35:24 +0100

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,25 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`, //greater than 1024px
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="tomato">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="papayawhip">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
