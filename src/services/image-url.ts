81a00dd478aacfed0e0a2f02beee26a6bb7a938a
Haitam Elgharras <elgharras.haitam@gmail.com>
2023-06-23 11:49:01 +0100

    Optimizing images

---
+++ src/services/image-url.ts
@@ -0,0 +1,9 @@
+// this component is used to optimize the image url by making the image smaller
+const getCroppedImageUrl = (url: string) => {
+  const splitingurl = url.split("/");
+  const index = splitingurl.indexOf("media");
+  splitingurl.splice(index + 1, 0, "crop/600/400");
+  return splitingurl.join("/");
+};
+
+export default getCroppedImageUrl;
