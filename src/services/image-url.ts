e57950580c10e7fc75b9691a964850d7109cd68a
Haitam Elgharras <elgharras.haitam@gmail.com>
2023-06-24 16:15:27 +0100

    handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,6 +1,8 @@
 // this component is used to optimize the image url by making the image smaller
+
+import noImage from "../assets/no-image-placeholder.webp";
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return noImage;
   const splitingurl = url.split("/");
   const index = splitingurl.indexOf("media");
   splitingurl.splice(index + 1, 0, "crop/600/400");
