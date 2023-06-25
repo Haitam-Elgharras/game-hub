a4a305f904e200921cf492b539ff0ed188e219a3
Haitam Elgharras <elgharras.haitam@gmail.com>
2023-06-25 12:32:23 +0100

    for adding static data

--- src/data/desc.ts
+++ src/data/desc.ts
@@ -1,5 +1,5 @@
 /*
     - From this file wwe can store static data retrieve it from here and use it in our components
       we can use this approach to store data that is not going to change often like :
-      * genres of games * platforms * etc...
+       * genres of games * platforms * etc...
  */
